dfdd959dd7b29800b3716b80c99aab2297ad0729
l3x-fx <kekskiller@email.com>
2023-09-07 10:59:44 +0200

    edited user signup/login response

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from "express"
 
-import { User, UserLogin } from "../models/user";
-import { UserStore } from "../models/user";
-import dotenv from "dotenv";
-import jwt, { Secret } from "jsonwebtoken";
-import { authorizeUser } from "../auth/auth";
+import { User, UserLogin } from "../models/user"
+import { UserStore } from "../models/user"
+import dotenv from "dotenv"
+import jwt, { Secret } from "jsonwebtoken"
+import { authorizeUser } from "../auth/auth"
 
-dotenv.config();
-const { TOKEN_SECRET } = process.env;
+dotenv.config()
+const { TOKEN_SECRET } = process.env
 
-export const store = new UserStore();
+export const store = new UserStore()
 
 const signup = async (req: Request, res: Response) => {
   try {
@@ -22,56 +22,56 @@ const signup = async (req: Request, res: Response) => {
       zip: 0,
       city: "",
       country: "",
-    };
-    const newUser = await store.signup(user);
+    }
+    const newUser = await store.signup(user)
 
-    let token = jwt.sign({ user: newUser }, TOKEN_SECRET as Secret);
-    res.json(token);
+    let token = jwt.sign({ user: newUser }, TOKEN_SECRET as Secret)
+    res.status(201).json({ user: newUser, token: token })
   } catch (error) {
-    console.log(error);
-    res.status(401).json({ error });
+    console.log(error)
+    res.status(401).json({ error })
   }
-};
+}
 
 const login = async (req: Request, res: Response) => {
   try {
-    const user: UserLogin = req.body.user;
-    const loggedInUser: User = await store.login(user);
-    let token = jwt.sign({ user: loggedInUser }, TOKEN_SECRET as Secret);
-    res.json(token);
+    const user: UserLogin = req.body.user
+    const loggedInUser: User = await store.login(user)
+    let token = jwt.sign({ userId: loggedInUser }, TOKEN_SECRET as Secret)
+    res.json({ user: loggedInUser, token: token })
   } catch (error) {
-    console.log(error);
-    res.status(401).json({ error });
+    console.log(error)
+    res.status(401).json({ error })
   }
-};
+}
 
 const showUserById = async (req: Request, res: Response) => {
   try {
-    const userId = parseInt(req.params.userId);
-    const user = await store.showUserById(userId);
-    res.json(user);
+    const userId = parseInt(req.params.userId)
+    const user = await store.showUserById(userId)
+    res.json(user)
   } catch (error) {
-    res.status(401).json({ error });
+    res.status(401).json({ error })
   }
-};
+}
 
 const editUserInfosById = async (req: Request, res: Response) => {
   try {
-    const user = req.body.user;
-    const userId = parseInt(req.params.userId);
-    const newUser = await store.editUserInfosById(user, userId);
-    res.json(newUser);
+    const user = req.body.user
+    const userId = parseInt(req.params.userId)
+    const newUser = await store.editUserInfosById(user, userId)
+    res.json(newUser)
   } catch (error) {
-    console.log(error);
-    res.status(401).json({ error });
+    console.log(error)
+    res.status(401).json({ error })
   }
-};
+}
 
 const users_routes = (app: express.Application) => {
-  app.post("/signup", signup);
-  app.get("/login", login);
-  app.get("/users/:userId", authorizeUser, showUserById);
-  app.put("/users/:userId", authorizeUser, editUserInfosById);
-};
+  app.post("/signup", signup)
+  app.get("/login", login)
+  app.get("/users/:userId", authorizeUser, showUserById)
+  app.put("/users/:userId", authorizeUser, editUserInfosById)
+}
 
-export default users_routes;
+export default users_routes
