0732848cddc15d9e254547bf161db274640b2d58
l3x-fx <kekskiller@email.com>
2023-09-28 14:30:30 +0200

    auth bugsearch

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,31 +1,31 @@
-import { Request, Response, NextFunction } from "express";
-import dotenv from "dotenv";
-import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express"
+import dotenv from "dotenv"
+import jwt, { JwtPayload, Secret } from "jsonwebtoken"
 
-dotenv.config();
-const { TOKEN_SECRET } = process.env;
+dotenv.config()
+const { TOKEN_SECRET } = process.env
 
 export const authorizeUser = (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token: string | undefined = req.headers.authorization;
+    const token: string | undefined = req.headers.authorization
     if (!token) {
-      return res.status(401).json({ error: "Unauthorized, token missing" });
+      return res.status(401).json({ error: "Unauthorized, token missing" })
     }
-    let authenticatedUser: JwtPayload | undefined;
+    let authenticatedUser: JwtPayload | undefined
     try {
-      authenticatedUser = jwt.verify(token.split(" ")[1], TOKEN_SECRET as Secret) as JwtPayload;
+      authenticatedUser = jwt.verify(token.split(" ")[1], TOKEN_SECRET as Secret) as JwtPayload
     } catch (verificationError) {
-      return res.status(403).json({ error: "Access denied, token invalid" });
+      return res.status(403).json({ error: "Access denied, token invalid" })
     }
 
-    const requestedUserId = parseInt(req.params.userId);
+    const requestedUserId = parseInt(req.params.userId)
 
     // @ts-ignore
     if ((requestedUserId as number) !== authenticatedUser.user.id) {
-      return res.status(403).json({ error: "Access denied, not authorized" });
+      return res.status(403).json({ error: "Access denied, not authorized" })
     }
-    next();
+    next()
   } catch (error) {
-    res.status(403).json({ error: "Access denied, not authorized" });
+    res.status(403).json({ error: "Access denied, not authorized" })
   }
-};
+}
