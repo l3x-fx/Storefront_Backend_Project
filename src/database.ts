f96a98e00993108524866d8fe7868cedb4685c80
l3x-fx <kekskiller@email.com>
2023-10-27 16:13:39 +0200

    dbaccess import

--- src/database.ts
+++ src/database.ts
@@ -1,37 +1,37 @@
-import dotenv from 'dotenv'
-import { Pool } from 'pg'
+import dotenv from "dotenv"
+import { Pool } from "pg"
 
 dotenv.config()
 
 const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_TEST_DB,
-    POSTGRES_USER,
-    POSTGRES_TEST_USER,
-    POSTGRES_PASSWORD, 
-    ENV
+  AZURE_POSTGRES_HOST,
+  AZURE_POSTGRES_DB,
+  POSTGRES_TEST_DB,
+  AZURE_POSTGRESQL_USER,
+  POSTGRES_TEST_USER,
+  AZURE_POSTGRESQL_PASSWORD,
+  ENV,
 } = process.env
 
-let Client 
+let Client
 console.log(ENV)
 
-if (ENV ==='test') {
-    Client = new Pool({
-        host: POSTGRES_HOST, 
-        database: POSTGRES_TEST_DB, 
-        user: POSTGRES_TEST_USER, 
-        password: POSTGRES_PASSWORD as string
-    })
+if (ENV === "test") {
+  Client = new Pool({
+    host: AZURE_POSTGRES_HOST,
+    database: POSTGRES_TEST_DB,
+    user: POSTGRES_TEST_USER,
+    password: AZURE_POSTGRESQL_PASSWORD as string,
+  })
 }
 
-if (ENV ==='dev') {
-    Client = new Pool({
-        host: POSTGRES_HOST, 
-        database: POSTGRES_DB, 
-        user: POSTGRES_USER, 
-        password: POSTGRES_PASSWORD as string
-    })
+if (ENV === "dev") {
+  Client = new Pool({
+    host: AZURE_POSTGRES_HOST,
+    database: AZURE_POSTGRES_DB,
+    user: AZURE_POSTGRESQL_USER,
+    password: AZURE_POSTGRESQL_PASSWORD as string,
+  })
 }
 
 export default Client
