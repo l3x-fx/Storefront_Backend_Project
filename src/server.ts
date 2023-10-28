a7009cf6780319eefa38ecf6bb11f56656bfa8ca
l3x-fx <kekskiller@email.com>
2023-10-28 14:33:47 +0200

    re-trigger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from "express"
+import bodyParser from "body-parser"
 //@ts-ignore
-import cors from 'cors'
-import users_routes from './handlers/users'
-import products_routes from './handlers/products'
-import orders_routes from './handlers/oders'
+import cors from "cors"
+import users_routes from "./handlers/users"
+import products_routes from "./handlers/products"
+import orders_routes from "./handlers/oders"
 
 export const app: express.Application = express()
 const address: string = "8080"
@@ -13,11 +13,11 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.listen(address, function () {
-    console.log(`starting app on: ${address}`)
+  console.log(`starting app on: ${address}`)
 })
 
-app.get('/', function (_req: Request, res: Response) {
-    res.send('Server is ready!')
+app.get("/", function (_req: Request, res: Response) {
+  res.send("Server is ready!")
 })
 
 users_routes(app)
