140b1d8b8839d9f25c4dc41ce9db4d9f5f004544
l3x-fx <kekskiller@email.com>
2023-10-30 19:47:09 +0100

    edit conn

--- src/database.ts
+++ src/database.ts
@@ -1,31 +1,31 @@
 import dotenv from "dotenv"
-import pg from "pg"
-import fs from "fs"
+import pg, { Pool } from "pg"
 
 dotenv.config()
 
 const { AZURE_POSTGRES_HOST, AZURE_POSTGRES_DB, AZURE_POSTGRESQL_USER, AZURE_POSTGRESQL_PASSWORD, AZURE_SSL } =
   process.env
 
-// let Client
+let Client
 
-// Client = new Pool({
-//   host: AZURE_POSTGRES_HOST,
-//   database: AZURE_POSTGRES_DB,
-//   user: AZURE_POSTGRESQL_USER,
-//   password: AZURE_POSTGRESQL_PASSWORD as string,
-//   port: 5432,
-//   ssl: true,
-// })
-
-// export default Client
-const config = {
+Client = new Pool({
   host: AZURE_POSTGRES_HOST,
-  user: AZURE_POSTGRESQL_USER,
-  password: AZURE_POSTGRESQL_PASSWORD,
   database: AZURE_POSTGRES_DB,
+  user: AZURE_POSTGRESQL_USER,
+  password: AZURE_POSTGRESQL_PASSWORD as string,
   port: 5432,
-  ssl: { ca: AZURE_SSL },
-}
+  ssl: true,
+})
 
-export const Client = new pg.Client(config)
+export default Client
+
+// const config = {
+//   host: AZURE_POSTGRES_HOST,
+//   user: AZURE_POSTGRESQL_USER,
+//   password: AZURE_POSTGRESQL_PASSWORD,
+//   database: AZURE_POSTGRES_DB,
+//   port: 5432,
+//   ssl: { ca: AZURE_SSL },
+// }
+
+// export const Client = new pg.Client(config)
